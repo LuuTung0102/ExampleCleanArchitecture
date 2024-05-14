848c490e9469878820f54cb88728669d40fdde30
WiBuMoney <cowsmile1911@gmail.com>
2024-05-14 14:23:49 +0700

    test

--- src/Application/Features/User/Handlers/CreateUserHandler.ts
+++ src/Application/Features/User/Handlers/CreateUserHandler.ts
@@ -16,31 +16,7 @@ export async function CreateUserHandler(data: any): Promise<CreateUserResponse|C
     const roleQueryData: any = {
         isDelete: false,
         isActive: true,
-    }
-
-    // Validate fields
-    // const emailError = validationUtils.validateEmail(email);
-    // const fullnameError = validationUtils.validateName(fullname);
-    // const passwordError = validationUtils.validatePassword(password);
-    // const phoneNumberError = validationUtils.validatePhoneNumber(phoneNumber);
-    // const usernameError = validationUtils.validateUsername(username);
-
-    // if (emailError || fullnameError || passwordError || phoneNumberError || usernameError) {
-    //   // Construct error message with appropriate separators
-    //   let errorMessage = '';
-    //   if (emailError) errorMessage += emailError + '; ';
-    //   if (fullnameError) errorMessage += fullnameError + '; ';
-    //   if (passwordError) errorMessage += passwordError + '; ';
-    //   if (phoneNumberError) errorMessage += phoneNumberError + '; ';
-    //   if (usernameError) errorMessage += usernameError + '; ';
-      
-    //   // Remove trailing ';' and return error response
-    //   errorMessage = errorMessage.trim().replace(/;+$/, '');
-    //   return new CreateUserResponse("Validation failed", 400, {}, errorMessage);
-    // }
-
-
-        
+    }       
     const role: any = await unitOfWork.roleRepository.getRoleByName("User", roleQueryData);
     const createUserRoleData: any = {
       email: email,
